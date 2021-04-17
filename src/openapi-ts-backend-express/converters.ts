6b1e8d8e020e0b0dea7332a86ef4a52bc23fb830
Keith Broughton <keithbro256@gmail.com>
2021-04-17 12:32:23 +1000

    Refactor

--- src/openapi-ts-backend-express/converters.ts
+++ src/openapi-ts-backend-express/converters.ts
@@ -1,14 +1,13 @@
-import { pipe, Refinement } from "fp-ts/lib/function";
+import { pipe } from "fp-ts/lib/function";
 import { filter } from "fp-ts/lib/Record";
 import { IncomingHttpHeaders } from "http";
 import { StringParams } from "openapi-ts-backend";
 import { OneOrMany } from "openapi-ts-backend/dist/utils";
 import { ParsedQs } from "qs";
 
-type ParsedQsValues = string | string[] | ParsedQs | ParsedQs[] | undefined;
-type OurRefinement = Refinement<ParsedQsValues, OneOrMany<string>>;
+type ParsedQsValue = string | string[] | ParsedQs | ParsedQs[] | undefined;
 
-const isStringBased: OurRefinement = (x): x is OneOrMany<string> => {
+const isStringBased = (x: ParsedQsValue): x is OneOrMany<string> => {
   if (typeof x === "string") return true;
   if (Array.isArray(x) && typeof x[0] === "string") return true;
 
