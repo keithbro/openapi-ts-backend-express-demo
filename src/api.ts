e5bfbb0454f48e1b85b1a580a2bcecde6ffeefca
Keith Broughton <hello@keithbro.com>
2021-04-18 11:56:09 +1000

    Update typescript

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,8 @@ import { OpenApi } from "openapi-ts-backend";
 import { OperationHandlers } from "./generated/types";
 import { showPetById, listPets, createPets } from "./pets_controller";
 
-// TODO: Fix annoying workaround
+// TODO: Fix annoying workaround where passing these directly in to
+// `register` causes a type error.
 const operations: OperationHandlers<unknown> = {
   showPetById,
   listPets,
